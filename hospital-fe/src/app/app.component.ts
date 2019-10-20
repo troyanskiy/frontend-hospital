201f72b664b2d792eaf0f0be66846082a7854c0a
ganqqwerty <katkov.juriy@gmail.com>
2019-10-20 20:56:06 +0200

    small reorganization

--- hospital-fe/src/app/app.component.ts
+++ hospital-fe/src/app/app.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
-import { forkJoin, Observable, zip } from 'rxjs';
-import { Drug } from 'hospital-lib/dist/state-machine.types';
-import { DrugsService } from './simulation/services/drugs.service';
-import { PatientsRegister, Quarantine } from 'hospital-lib';
-import { PatientsDataService } from './simulation/services/patients-data.service';
-import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { QuarantineService, Simulation } from './simulation/services/quarantine.service';
+import { share } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -16,13 +12,11 @@ export class AppComponent {
   title = 'hospital-fe';
   results$: Observable<Simulation>;
 
-
   constructor(private quarantineService: QuarantineService) {
 
   }
 
-
   runSimulation() {
-    this.results$ = this.quarantineService.runSimulation();
+    this.results$ = this.quarantineService.runSimulation().pipe(share());
   }
 }
