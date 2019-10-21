96f8fa80becda53047393b7eed9bbcdf05ea134d
ganqqwerty <katkov.juriy@gmail.com>
2019-10-21 23:54:21 +0200

    refactor

--- hospital-fe/src/app/app.component.ts
+++ hospital-fe/src/app/app.component.ts
@@ -5,7 +5,7 @@ import {
   QuarantineService
 } from './simulation/services/quarantine.service';
 import { HistoryService } from './simulation/services/history.service';
-import { filter, partition, switchMapTo, takeUntil } from 'rxjs/operators';
+import { partition, switchMapTo, takeUntil } from 'rxjs/operators';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 
 @Component({
@@ -14,27 +14,22 @@ import { MatSlideToggleChange } from '@angular/material/slide-toggle';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hospital-fe';
   history$: Observable<BeforeAfterStatistic[]>;
-  timer$ = interval(1000);
+  automaticCalculationInterval = 1000;
 
   private newSimulationClicks$ = new Subject();
   private autoMode$ = new Subject<MatSlideToggleChange>();
 
   constructor(private quarantineService: QuarantineService,
               private historyService: HistoryService) {
-    const result$: Observable<BeforeAfterStatistic> = this.setUpStartStopEvents().pipe(switchMapTo(quarantineService.runSimulation()));
+    const result$ = this.setUpStartStopEvents().pipe(switchMapTo(quarantineService.runSimulation()));
     this.history$ = this.historyService.getHistory(result$);
   }
 
   setUpStartStopEvents(): Observable<void> {
     const [autoModeStartClicks$, autoModeStopClicks$] = partition((evt: MatSlideToggleChange) => evt.checked)(this.autoMode$);
-    const intervalThatStops$ = interval(1000).pipe(
-      takeUntil(autoModeStopClicks$)
-    );
-    const autoModeStartingTrigger$ = autoModeStartClicks$.pipe(
-      switchMapTo(intervalThatStops$),
-    );
+    const intervalThatStops$ = interval(this.automaticCalculationInterval).pipe(takeUntil(autoModeStopClicks$));
+    const autoModeStartingTrigger$ = autoModeStartClicks$.pipe(switchMapTo(intervalThatStops$));
     return merge<void>(this.newSimulationClicks$, autoModeStartingTrigger$);
   }
 
