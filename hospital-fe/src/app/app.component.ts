e3a332cf74cd0181b689eccce9a13210024b8cb3
ganqqwerty <katkov.juriy@gmail.com>
2019-10-22 00:30:27 +0200

    removed subjects

--- hospital-fe/src/app/app.component.ts
+++ hospital-fe/src/app/app.component.ts
@@ -1,43 +1,46 @@
-import { Component } from '@angular/core';
-import { interval, merge, Observable, Subject } from 'rxjs';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { fromEvent, interval, merge, Observable } from 'rxjs';
 import {
   BeforeAfterStatistic,
   QuarantineService
 } from './simulation/services/quarantine.service';
 import { HistoryService } from './simulation/services/history.service';
 import { partition, switchMapTo, takeUntil } from 'rxjs/operators';
-import { MatSlideToggleChange } from '@angular/material/slide-toggle';
+import { MatSlideToggle, MatSlideToggleChange } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
   history$: Observable<BeforeAfterStatistic[]>;
+
   automaticCalculationInterval = 1000;
 
-  private newSimulationClicks$ = new Subject();
-  private autoMode$ = new Subject<MatSlideToggleChange>();
+  @ViewChild('newSimulationButton', { static: false, read: ElementRef })
+  newSimulationButton: ElementRef;
+
+  @ViewChild('autoModeSlideToggle', { static: false })
+  autoModeSlideToggle: MatSlideToggle;
 
   constructor(private quarantineService: QuarantineService,
               private historyService: HistoryService) {
-    const result$ = this.setUpStartStopEvents().pipe(switchMapTo(quarantineService.runSimulation()));
+  }
+
+  ngAfterViewInit(): void {
+    const result$ = this.setUpStartStopEvents().pipe(switchMapTo(this.quarantineService.runSimulation()));
     this.history$ = this.historyService.getHistory(result$);
   }
 
   setUpStartStopEvents(): Observable<void> {
-    const [autoModeStartClicks$, autoModeStopClicks$] = partition((evt: MatSlideToggleChange) => evt.checked)(this.autoMode$);
+    const autoMode$ = this.autoModeSlideToggle.change;
+    const newSimulationClicks$ = fromEvent(this.newSimulationButton.nativeElement, 'click');
+    const [autoModeStartClicks$, autoModeStopClicks$] = partition((evt: MatSlideToggleChange) => evt.checked)(autoMode$);
     const intervalThatStops$ = interval(this.automaticCalculationInterval).pipe(takeUntil(autoModeStopClicks$));
     const autoModeStartingTrigger$ = autoModeStartClicks$.pipe(switchMapTo(intervalThatStops$));
-    return merge<void>(this.newSimulationClicks$, autoModeStartingTrigger$);
+    return merge<void>(newSimulationClicks$, autoModeStartingTrigger$);
   }
 
-  runSimulation() {
-    this.newSimulationClicks$.next();
-  }
-
-  automaticCheckboxValueChange(evt: MatSlideToggleChange) {
-    this.autoMode$.next(evt);
-  }
 }
