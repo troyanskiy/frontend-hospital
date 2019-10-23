8d1f384cc05daf8f957e8aa722efc1a6d39d9331
ganqqwerty <katkov.juriy@gmail.com>
2019-10-23 14:21:47 +0200

    drugs short

--- hospital-fe/src/app/simulation/drugs-short/drugs-short.component.ts
+++ hospital-fe/src/app/simulation/drugs-short/drugs-short.component.ts
@@ -1,25 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Drug } from 'hospital-lib';
+import { Drug, possibleDrugs } from 'hospital-lib';
 
 @Component({
   selector: 'app-drugs-short',
   templateUrl: './drugs-short.component.html',
+  styleUrls: ['./drugs-short.component.scss']
 })
-export class DrugsShortComponent implements OnInit {
+export class DrugsShortComponent {
 
   @Input()
   drugs: Drug[];
 
-  drugsFormatted: string;
-
-  ngOnInit() {
-    this.drugsFormatted = this.formatDrugs();
-  }
-
-  //fixme convert to pipe
-  private formatDrugs(): string {
-    return this.drugs.length ? this.drugs
-      .map(drugFullName => drugFullName.substring(0, 3))
-      .join(', ') : 'No drugs given';
-  }
+  possibleDrugs = possibleDrugs;
 }
